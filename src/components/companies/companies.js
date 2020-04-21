4724c7978b26d664ba47250301c83ee7712b6ac8
julie <julie@julies-MacBook-Pro.local>
2020-04-20 19:22:34 -0700

    remove and modified form

--- src/components/companies/companies.js
+++ src/components/companies/companies.js
@@ -1,30 +1,10 @@
 import React from "react";
-import Wishlist from "./Wishlist/Wishlist"
-import Applied from "./Applied/Applied"
-import Interviews from "./Interviews/Interviews"
-import Offers from "./Offers/Offers"
-import { Col, Row } from "react-bootstrap"
+import CompaniesForm from "./CompaniesForm"
 
 const CompaniesApplications = () => {
   return (
     <div>
-        <Row>
-            <Col>
-                <Wishlist />
-            </Col>
-
-            <Col>
-                <Applied />
-            </Col>
-
-            <Col>
-                <Interviews />
-            </Col>
-
-            <Col>
-                <Offers />
-            </Col>
-        </Row>
+        <CompaniesForm />
     </div>
   );
 };
