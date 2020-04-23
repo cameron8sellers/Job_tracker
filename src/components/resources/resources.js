8eaa352f95ea45a1db8a4867171926f5a272177f
Cameron Sellers <cameronsmac@Camerons-MacBook-Pro.local>
2020-04-23 09:07:39 -0600

    cards made connected to api

--- src/components/resources/resources.js
+++ src/components/resources/resources.js
@@ -1,5 +1,5 @@
-import React from "react";
-import resourceSeed from "../SeedData/resourceSeed";
+import React, { useState, useEffect, useContext } from "react";
+import { TrackerContext } from "../../App";
 import { Card } from "reactstrap";
 import {
   getResources,
@@ -8,110 +8,107 @@ import {
 } from "../../services/api-helper-Resources";
 import "./resources.css";
 
-const resourcesPage = () => {
-  let list = resourceSeed.map(item => {
+const ResourcesPage = () => {
+  const [resources, setResources] = useState("");
+  const [isLoading, setIsLoading] = useState(true);
+
+  const sharedStates = useContext(TrackerContext);
+  console.log(sharedStates.resources);
+
+  const jobBoards = sharedStates.resources["Job Boards"].map(
+    (resource, index) => {
+      return (
+        <div>
+          <a href={resource.url}>
+            <li>{resource.name}</li>
+          </a>
+        </div>
+      );
+    }
+  );
+
+  const personalBranding = sharedStates.resources["Personal Branding"].map(
+    (resource, index) => {
+      return (
+        <div>
+          <a href={resource.url}>
+            <li>{resource.name}</li>
+          </a>
+        </div>
+      );
+    }
+  );
+
+  const resume = sharedStates.resources["Resume And Cover Letter"].map(
+    (resource, index) => {
+      return (
+        <div>
+          <a href={resource.url}>
+            <li>{resource.name}</li>
+          </a>
+        </div>
+      );
+    }
+  );
+
+  const interview = sharedStates.resources["Interviewing"].map(
+    (resource, index) => {
+      return (
+        <div>
+          <a href={resource.url}>
+            <li>{resource.name}</li>
+          </a>
+        </div>
+      );
+    }
+  );
+  const salary = sharedStates.resources["Salary Negotiation"].map(
+    (resource, index) => {
+      return (
+        <div>
+          <a href={resource.url}>
+            <li>{resource.name}</li>
+          </a>
+        </div>
+      );
+    }
+  );
+  const tech = sharedStates.resources["Technical"].map((resource, index) => {
     return (
-      <div className="resources-main">
-        <Card className="job-head">
-          <h1>Job Boards</h1>
-        </Card>
-        <Card className="jobBoard-card">
-          <p>
-            {item.jobBoards.map(board => {
-              return (
-                <ul>
-                  <li>
-                    <a href={board.url}>{board.name}</a>
-                  </li>
-                </ul>
-              );
-            })}
-          </p>
-        </Card>
-        <Card className="personal-head">
-          <h1>Personal Branding</h1>
-        </Card>
-        <Card className="personalBranding-card">
-          <p>
-            {item.personalBranding.map(brand => {
-              return (
-                <ul>
-                  <li>
-                    <a href={brand.url}>{brand.name}</a>
-                  </li>
-                </ul>
-              );
-            })}
-          </p>
-        </Card>
-        <Card className="resume-head">
-          <h1>Resume and Cover Letter </h1>
-        </Card>
-        <Card className="resume-card">
-          <p>
-            {item.resumeAndCoverLetter.map(item => {
-              return (
-                <ul>
-                  <li>
-                    <a href={item.url}>{item.name}</a>
-                  </li>
-                </ul>
-              );
-            })}
-          </p>
-        </Card>
-        <Card className="interview-head">
-          <h1>Interviewing</h1>
-        </Card>
-        <Card className="interview-card">
-          <p>
-            {item.interviewing.map(item => {
-              return (
-                <ul>
-                  <li>
-                    <a href={item.url}>{item.name}</a>
-                  </li>
-                </ul>
-              );
-            })}
-          </p>
-        </Card>
-        <Card className="salary-head">
-          <h1>Salary Negotiation</h1>
-        </Card>
-        <Card className="salary-card">
-          <p>
-            {item.salaryNegotiation.map(item => {
-              return (
-                <ul>
-                  <li>
-                    <a href={item.url}>{item.name}</a>
-                  </li>
-                </ul>
-              );
-            })}
-          </p>
-        </Card>
-        <Card className="tech-head">
-          <h1>Technical</h1>
-        </Card>
-        <Card className="tech-card">
-          <p>
-            {item.technical.map(item => {
-              return (
-                <ul>
-                  <li>
-                    <a href={item.url}>{item.name}</a>
-                  </li>
-                </ul>
-              );
-            })}
-          </p>
-        </Card>
+      <div>
+        <a href={resource.url}>
+          <li>{resource.name}</li>
+        </a>
       </div>
     );
   });
-  return <Card>{list}</Card>;
+  return (
+    <div className="resources-main">
+      <Card className="job-head">
+        <h1> Job Boards</h1>
+      </Card>
+      <Card className="jobBoard-card">{jobBoards}</Card>
+      <Card className="personal-head">
+        <h1>Personal Branding</h1>
+      </Card>
+      <Card className="personalBranding-card">{personalBranding}</Card>
+      <Card className="resume-head">
+        <h1>Resume and Cover Letter</h1>
+      </Card>
+      <Card className="resume-card">{resume}</Card>
+      <Card className="interview-head">
+        <h1>Interviewing</h1>
+      </Card>
+      <Card className="interview-card">{interview}</Card>
+      <Card className="salary-head">
+        <h1>Salary Negotiation</h1>
+      </Card>
+      <Card className="salary-card">{salary}</Card>
+      <Card className="tech-head">
+        <h1>Technical</h1>
+      </Card>
+      <Card className="tech-card">{tech}</Card>
+    </div>
+  );
 };
-
-export default resourcesPage;
+export default ResourcesPage;
