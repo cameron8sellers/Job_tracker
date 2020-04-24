3424be7b7621af5ebb7df22c509f8cb20ff124a6
Cameron Sellers <cameronsmac@Camerons-MacBook-Pro.local>
2020-04-24 09:23:33 -0600

    last css change

--- src/components/resources/resources.js
+++ src/components/resources/resources.js
@@ -81,27 +81,27 @@ const ResourcesPage = () => {
     return (
       <div className="resources-main">
         <Card className="job-head">
-          <h1> Job Boards</h1>
+          <h1 className="card-head"> Job Boards</h1>
         </Card>
         <Card className="jobBoard-card">{jobBoards}</Card>
         <Card className="personal-head">
-          <h1>Personal Branding</h1>
+          <h1 className="card-head">Personal Branding</h1>
         </Card>
         <Card className="personalBranding-card">{personalBranding}</Card>
         <Card className="resume-head">
-          <h1>Resume and Cover Letter</h1>
+          <h1 className="card-head">Resume and Cover Letter</h1>
         </Card>
         <Card className="resume-card">{resume}</Card>
         <Card className="interview-head">
-          <h1>Interviewing</h1>
+          <h1 className="card-head">Interviewing</h1>
         </Card>
         <Card className="interview-card">{interview}</Card>
         <Card className="salary-head">
-          <h1>Salary Negotiation</h1>
+          <h1 className="card-head">Salary Negotiation</h1>
         </Card>
         <Card className="salary-card">{salary}</Card>
         <Card className="tech-head">
-          <h1>Technical</h1>
+          <h1 className="card-head">Technical</h1>
         </Card>
         <Card className="tech-card">{tech}</Card>
       </div>
